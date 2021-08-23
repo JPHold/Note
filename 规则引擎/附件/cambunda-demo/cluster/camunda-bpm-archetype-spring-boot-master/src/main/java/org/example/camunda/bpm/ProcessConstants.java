f3609f4d2546631c9fef779e8db6d77432e1420b
JpHold <1208405111@qq.com>
2021-08-23 16:51:12 +0800

    obsidian-git自动提交: 2021-08-23 16:51:12，514

---
+++ 规则引擎/附件/cambunda-demo/cluster/camunda-bpm-archetype-spring-boot-master/src/main/java/org/example/camunda/bpm/ProcessConstants.java
@@ -0,0 +1,7 @@
+package org.example.camunda.bpm;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "camunda-process-application"; // BPMN Process ID
+
+}
---
+++ 规则引擎/附件/cambunda-demo/cluster/camunda-bpm-archetype-spring-boot-master/src/test/java/org/example/camunda/bpm/ProcessUnitTest.java
@@ -0,0 +1,81 @@
+package org.example.camunda.bpm;
+
+import java.util.Arrays;
+import java.sql.SQLException;
+import javax.annotation.PostConstruct;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.Variables.SerializationDataFormats;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProcessUnitTest {
+
+  @Autowired
+  private ProcessEngine processEngine;
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @ClassRule
+  @Rule
+  public static ProcessEngineRule rule;
+
+  @PostConstruct
+  void initRule() {
+    rule = TestCoverageProcessEngineRuleBuilder.create(processEngine).build();
+  }
+
+  @Before
+  public void setup() {
+    init(processEngine);
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn") // only required for process test coverage
+  public void testHappyPath() throws SQLException {
+    // Drive the process by API and assert correct behavior by camunda-bpm-assert
+
+    ObjectValue documents = Variables
+      .objectValue(Arrays.asList(new String[]{"one", "two", "three"}))
+      .serializationDataFormat(SerializationDataFormats.JSON)
+      .create();
+
+    ProcessInstance processInstance = runtimeService()
+        .createProcessInstanceByKey(ProcessConstants.PROCESS_DEFINITION_KEY)
+        .businessKey("23")
+        .setVariable("documents", documents)
+        .execute();
+
+    assertThat(processInstance).isWaitingAt("UserTask_Approve");
+
+    // To inspect the DB connect your browser to: http://localhost:8080/h2-console/
+
+  }
+
+}
