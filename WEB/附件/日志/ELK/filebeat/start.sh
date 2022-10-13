45ac3ef8914e462a638042975130b052b765b132
hjp <1208405111@qq.com>
2022-10-13 16:27:12 +0800

    obsidian-git自动提交: 2022-10-13 16:27:12，1

--- WEB/附件/日志/ELK/filebeat/start.sh
+++ WEB/附件/日志/ELK/filebeat/start.sh
@@ -5,5 +5,5 @@ docker run -d --network host \
   --user root \
   -e -strict.perms=false \
   --volume="/home/kong-log:/usr/share/filebeat/file-log" \
-  --volume="/home/software/elk/filbeat/data:/usr/share/filebeat/data" \
+  --volume="/home/software/elk/filebeat/data:/usr/share/filebeat/data" \
   local.harbor.com/library/filebeat-custom:6.7.0
